d235c05b650a869c029d7dd8e2d99f12d586e282
Peter Freitas <opeterfreitas@gmail.com>
2023-05-14 13:03:49 -0300

    Implementação Spring Security - Antes e depois v 5.7

---
+++ src/main/java/com/opeterfreitas/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.opeterfreitas.api.parkingcontrol.configs.security;
+
+import com.opeterfreitas.api.parkingcontrol.models.UserModel;
+import com.opeterfreitas.api.parkingcontrol.repositories.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel userModel = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+        return new User(userModel.getUsername(), userModel.getPassword(), true, true, true, true, userModel.getAuthorities());
+    }
+}
---
+++ src/main/java/com/opeterfreitas/api/parkingcontrol/enums/RoleName.java
@@ -0,0 +1,6 @@
+package com.opeterfreitas.api.parkingcontrol.enums;
+
+public enum RoleName {
+    ROLE_ADMIN,
+    ROLE_USER;
+}
